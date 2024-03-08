d0c0979d1af173a072c2d613f1d7c388bef47fc8
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-08 18:00:48 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState,useEffect } from "react";
+
 
 function App() {
+  const [recipes,setRecipes]=useState([]);
+  const [value,Setvalue]=useState("")
+  const [disp,setDisp]=useState(false)
+
+useEffect(()=>{
+  if(value){
+    setDisp(true)
+  }
+  else{
+    setDisp(false)
+  }
+},[value])
+
+useEffect(()=>{
+  getRecipes()
+},[])
+
+const getRecipes=async ()=>{
+  const response=await fetch("./foodrecipe.json");
+  const data=await response.json();
+  setRecipes(data)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Content">
+    <input type="text" placeholder="Search a food recipe..." onChange={(e)=>Setvalue(e.target.value)}/>
+    {
+    disp && <div className="container">
+      {recipes.map((recipe)=>{
+      return(
+      recipe.name.toUpperCase().indexOf(value.toUpperCase())!==-1 && <div className="Recipes" key={Math.random()}>
+        <h1>{recipe.name}</h1>
+        <img src={recipe.imageURL} alt="alternative" height="300" />
+
+        <article>
+          <div className="header">Ingredients:</div>
+          {recipe.ingredients.map(item=>{
+            return(<div key={Math.random()} className="recipeContainer">
+              <li>{item.quantity} {item.name}</li>
+            </div>)
+          })}
+          </article>
+
+          <article>
+            <div className="header">Steps:</div>
+            {recipe.steps.map(item=>{
+              return(<li className="steps" key={Math.random()}>{item}</li>)
+            })}
+          </article>
+        <hr />
+      </div>)
+    })}
     </div>
+    }
+  </div>
+
   );
 }
 
